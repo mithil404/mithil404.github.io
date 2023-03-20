9fa63e419de9f45ae549e9dad78f98b92e775c44
Mithil Salunkhe <83882958+mithil404@users.noreply.github.com>
2023-03-20 23:00:43 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,9 @@
+var x = 0;
+function inc() {  
+    x++;
+    document.getElementById("smthg").innerHTML = x;
+} 
+function reset() {  
+    x = 0;
+    document.getElementById("smthg").innerHTML = x;
+}
