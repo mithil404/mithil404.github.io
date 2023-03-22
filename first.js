f673c218d5e7ff4e21ffc2e6e49aadb2f5d7e5b0
mithil404 <mithilsalunke@protonmail.com>
2023-03-22 12:40:43 +0530

    change display to grid for buttons

--- first.js
+++ first.js
@@ -1,9 +1,9 @@
-var x = 0;
+var x = 0
 function inc() {  
-    x++;
-    document.getElementById("smthg").innerHTML = x;
+    x++
+    document.getElementById("smthg").innerHTML = x
 } 
 function reset() {  
-    x = 0;
-    document.getElementById("smthg").innerHTML = x;
+    x = 0
+    document.getElementById("smthg").innerHTML = x
 }
